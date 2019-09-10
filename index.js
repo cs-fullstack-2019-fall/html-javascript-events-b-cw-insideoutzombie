f88c78235b4f21e3ae1c6078e3dea2cc2228cd21
insideoutzombie <david@web3devs.com>
2019-09-10 15:55:32 -0500

    attempted

---
+++ index.js
@@ -0,0 +1,50 @@
+let codeCrewString = "Code School is a hands-on class designed to train individuals to be entry-level software developers within a six-month course. The minimum education requirement is at least a high school diploma or GED. Students work in a small classroom setting and use real-word technologies to learn the fundamentals of coding, app development, the leadership, and the life skills needed for a successful career and job market competitiveness.";
+
+let myArray = [
+  "some string",
+  "another string",
+  "third string "
+];
+
+let randomString = myArray[Math.floor(Math.random()*myArray.length)];
+console.log(randomString);
+
+
+function myFunction() {
+  let x = document.getElementById("myTextarea").value;
+  document.getElementById("blanklabel").innerHTML = codeCrewString;
+  console.log(randomString)
+}
+
+
+function resetFunction() {
+  let x = document.getElementById("myTextarea").value;
+  document.getElementById("blanklabel").innerHTML = "";
+}
+
+
+// JUNK PILE BELOW ##################################
+
+// let x = document.getElementById("myTextarea").value;
+//   x.onkeydown = function(e) {
+//   console.log(randomString);
+// }
+
+
+// let inputElement = document.getElementsByTagName("textarea");
+//
+// let text2 = document.getElementsByTagName("label");
+//
+// inputElement.onkeydown = function(e){
+//   console.log(e.target.value);
+// }
+
+// let x = document.getElementById("myTextarea").value;
+// x.onkeydown = function(e){
+//   console.log(e.target.value);
+// }
+
+//
+// inputElement.onclick = function (){
+//     text2.innerText = "Changed";
+// };
